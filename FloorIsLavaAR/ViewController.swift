be242c444eb28aca2495272c4e941ffee1bfdb28
Dean Wahle <60762514+DeanWahle@users.noreply.github.com>
2022-01-20 21:54:54 -0500

    Initial Commit

---
+++ FloorIsLavaAR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FloorIsLavaAR
+//
+//  Created by Dean Wahle on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
